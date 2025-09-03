573fa035a0465dad88d770414d88c59685494ef5
Pito Salas <pitosalas@gmail.com>
2025-09-03 19:41:34 -0400

    work in progress

--- linorobot2_description/launch/description.launch.py
+++ linorobot2_description/launch/description.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
     robot_base = os.getenv('LINOROBOT2_BASE')
 
     urdf_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_description"), "urdf/robots", f"{robot_base}.urdf.xacro"]
+        [FindPackageShare("linorobot2_description"), "urdf", "dome1.urdf"]
     )
 
     rviz_config_path = PathJoinSubstitution(
