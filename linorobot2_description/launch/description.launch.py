b5a3886f0289d74e9f9a0def530c948a0330705c
Pito Salas <pitosalas@gmail.com>
2025-09-04 15:32:44 -0400

    work in progress

--- linorobot2_description/launch/description.launch.py
+++ linorobot2_description/launch/description.launch.py
@@ -24,8 +24,12 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     robot_base = os.getenv('LINOROBOT2_BASE')
 
+#     urdf_path = PathJoinSubstitution(
+#     [FindPackageShare("linorobot2_description"), "urdf/robots", f"{robot_base}.urdf.xacro"]
+# )
+
     urdf_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_description"), "urdf", "dome1.urdf"]
+        [FindPackageShare("linorobot2_description"), "urdf", "dome1.urdf.xacro"]
     )
 
     rviz_config_path = PathJoinSubstitution(
