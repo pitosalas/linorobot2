7e8307520a8336c26f354eb5ba979d1ca9768730
Pito Salas <pitosalas@users.noreply.github.com>
2025-11-24 11:01:52 -0500

    update

--- linorobot2_navigation/launch/navigation.launch.py
+++ linorobot2_navigation/launch/navigation.launch.py
@@ -14,53 +14,44 @@
 # limitations under the License.
 
 import os
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.substitutions import FindPackageShare
-
+from launch_ros.actions import Node
 MAP_NAME = "playground"  # change to the name of your own map here
-from launch.actions import LogInfo
 
 
 def generate_launch_description():
-    depth_sensor = os.getenv("LINOROBOT2_DEPTH_SENSOR", "")
+    depth_sensor = os.getenv('LINOROBOT2_DEPTH_SENSOR', '')
 
     nav2_launch_path = PathJoinSubstitution(
-        [FindPackageShare("nav2_bringup"), "launch", "bringup_launch.py"]
+        [FindPackageShare('nav2_bringup'), 'launch', 'bringup_launch.py']
     )
 
     rviz_config_path = PathJoinSubstitution(
-        [
-            FindPackageShare("linorobot2_navigation"),
-            "rviz",
-            "linorobot2_navigation.rviz",
-        ]
+        [FindPackageShare('linorobot2_navigation'), 'rviz', 'linorobot2_navigation.rviz']
     )
 
     default_map_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_navigation"), "maps", f"{MAP_NAME}.yaml"]
+        [FindPackageShare('linorobot2_navigation'), 'maps', f'{MAP_NAME}.yaml']
     )
 
     nav2_config_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_navigation"), "config", "navigation.yaml"]
+        [FindPackageShare('linorobot2_navigation'), 'config', 'navigation.yaml']
     )
 
     nav2_sim_config_path = PathJoinSubstitution(
-        [FindPackageShare("linorobot2_navigation"), "config", "navigation_sim.yaml"]
+        [FindPackageShare('linorobot2_navigation'), 'config', 'navigation_sim.yaml']
     )
-
-<<<<<<< HEAD
 
     return LaunchDescription([
         DeclareLaunchArgument(
             name='sim', 
             default_value='false',
-            description='Enable use_sime_time to true',
+            description='Enable use_sime_time to true'
         ),
 
         DeclareLaunchArgument(
@@ -111,56 +102,3 @@ def generate_launch_description():
             parameters=[{'use_sim_time': LaunchConfiguration("sim")}]
         )
     ])
-=======
-    return LaunchDescription(
-        [
-            LogInfo(msg=["Using params_file: ", LaunchConfiguration("config")]),
-            DeclareLaunchArgument(
-                name="sim",
-                default_value="false",
-                description="Enable use_sime_time to true",
-            ),
-            DeclareLaunchArgument(
-                name="rviz", default_value="false", description="Run rviz"
-            ),
-            DeclareLaunchArgument(
-                name="map",
-                default_value=default_map_path,
-                description="Navigation map path",
-            ),
-            DeclareLaunchArgument(
-                name="config",
-                default_value=nav2_config_path,
-                description="Nav2 parameters file",
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(nav2_launch_path),
-                condition=UnlessCondition(LaunchConfiguration("sim")),
-                launch_arguments={
-                    "map": LaunchConfiguration("map"),
-                    "use_sim_time": LaunchConfiguration("sim"),
-                    "params_file": LaunchConfiguration("config"),
-                }.items(),
-            ),
-            # Pito Note:There should be a different config for the sim mode
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(nav2_launch_path),
-                condition=IfCondition(LaunchConfiguration("sim")),
-                launch_arguments={
-                    "map": LaunchConfiguration("map"),
-                    "use_sim_time": LaunchConfiguration("sim"),
-                    "params_file": LaunchConfiguration("config"),
-                }.items(),
-            ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                output="screen",
-                arguments=["-d", rviz_config_path],
-                condition=IfCondition(LaunchConfiguration("rviz")),
-                parameters=[{"use_sim_time": LaunchConfiguration("sim")}],
-            ),
-        ]
-    )
->>>>>>> fd4a203c1f836764464b1f151d089817c6b18167
